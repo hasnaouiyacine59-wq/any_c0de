0dcaca3dcaeb5aa1c3cc093573fd28282b87e431
oo33 <hasnaouiyacine59@gmail.com>
2026-04-14 23:07:57 +0100

    fix_bulk1

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,13 +4,6 @@ git pull
 REGISTRY="quay.io/mylastres0rt05_redhat"
 docker login -u='mylastres0rt05_redhat' -p='FblabXC4LK1oW8SekCVMi+98cxukLiBephztGlutNdxQSoLVacTSvDbxZi9/qrbf' quay.io
 
-VERSIONS=(
-  "v1.44.0-jammy v1.44.0 v1.44 124"
-  # "v1.43.0-jammy v1.43.0 v1.43 123"
-  # "v1.42.0-jammy v1.42.0 v1.42 122"
-  # "v1.41.0-jammy v1.41.0 v1.41 121"
-  # "v1.40.0-jammy v1.40.0 v1.40 120"
-)
 
 echo "==> Building nova_dromidia-proxy"
 docker build -t "${REGISTRY}/nova_dromidia-proxy:latest" tor-proxy/
