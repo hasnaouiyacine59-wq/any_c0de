facf372df6b12d4126d3a01df837c1c1a86aee22
oo33 <hasnaouiyacine59@gmail.com>
2026-05-07 15:58:57 +0100

    fix_bulk1

--- tor-proxy/start.sh
+++ tor-proxy/start.sh
@@ -3,8 +3,8 @@ SOCKS_PORT=${SOCKS_PORT:-9050}
 CONTROL_PORT=${CONTROL_PORT:-9051}
 
 sed "s/\${SOCKS_PORT}/$SOCKS_PORT/g; s/\${CONTROL_PORT}/$CONTROL_PORT/g" \
-    /etc/tor/torrc.template > /etc/tor/torrc
+    /etc/tor/torrc.template > /tmp/torrc
 
-tor -f /etc/tor/torrc &
+tor -f /tmp/torrc &
 sleep 5
 python3 /app/api.py
